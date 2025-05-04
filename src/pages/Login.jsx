54e05888c6db7356bcfbd07d5aacfa5e8f7c6200
Breyner012 <bhernandezos@cesde.net>
2025-05-03 23:16:30 -0500

    Inicio del Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,43 @@
+import "../css/Login.css";
+
 const Login = () => {
   return (
-    <div className="login">
-      <h1>Login</h1>
-      <form>
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
-    </div>
+    <section className="login__container">
+      <img src="/public/ChatGPT Image 3 may 2025, 19_15_47.png" alt="" />
+      <div className="user__container">
+        <h1>Bienvenido a JolyGuacamoly!</h1>
+        <p>Inicia sesión o crea tu cuenta para empezar a saborear.</p>
+        <div className="form__container">
+          <form action="" className="login">
+            <div className="form__container--input">
+              <label htmlFor="email">Correo electrónico</label>
+              <input type="email" name="email" id="email" required />
+            </div>
+            <div className="form__container--input">
+              <label htmlFor="password">Contraseña</label>
+              <input type="password" name="password" id="password" required />
+            </div>
+            <button type="submit">Iniciar sesión</button>
+          </form>
+
+          {/* <form action="" className="register">
+            <div className="form__container--input">
+              <label htmlFor="name">Nombre</label>
+              <input type="text" name="name" id="name" required />
+            </div>
+            <div className="form__container--input">
+              <label htmlFor="email">Correo electrónico</label>
+              <input type="email" name="email" id="email" required />
+            </div>
+            <div className="form__container--input">
+              <label htmlFor="password">Contraseña</label>
+              <input type="password" name="password" id="password" required />
+            </div>
+            <button type="submit">Crear cuenta</button>
+          </form> */}
+        </div>
+      </div>
+    </section>
   );
 }
 
